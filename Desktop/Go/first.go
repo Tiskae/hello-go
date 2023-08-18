c11f6c8ace030096ee2c82680e49b712767e4853
Tiskae <macbook@Tiskaes-MacBook-Pro.local>
2023-08-18 19:08:39 +0100

    update: dereferencing

--- Desktop/Go/first.go
+++ Desktop/Go/first.go
@@ -8,4 +8,12 @@ const num2 int = 9
 func main() {
 	total := num1 + num2
 	fmt.Println("The sum of the two numbers is ", total)
+
+	test_num := 7
+	test_num_pointer := &test_num
+
+	fmt.Println((test_num_pointer))
+	// Dereferencing or indirecting
+	*test_num_pointer = 77
+	fmt.Println(*test_num_pointer)
 }	
