b586ac99f7031dbcfe5b37fbe8c45f38753d685b
Tiskae <adedokuntobiloba100@gmail.com>
2025-08-19 15:59:36 +0100

    basic refresher complete

--- refresher/new.go
+++ refresher/new.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 )
+
+func multipleArgsFunc(args... int)  {
+	fmt.Println(args)
+}
 
 func main() {
 	fmt.Println("Hello World!")
@@ -32,10 +37,50 @@ func main() {
 	fmt.Printf("My name is %s and the type is %T. My account balance is %+d \n", "Tiskae", "Tiskae", 23_000)
 	fmt.Println(math.Abs(-10000))
 
-	var arr1 = [5]int{1, 2, 3}
+	var arr1 = []int{1, 2, 3}
 	var arr2 = []int{1, 2, 3}
 	cars := [7]string{"BMW", "Merecdes", "Porsche", "Audi", "Volkswagen", "Koenigesgg", "Lamborghini"}
+	emptyArr := [5]string{1: "Nothing", 4: "Void"}
+	newArr := append(arr1, arr2...)
 
 	fmt.Println(arr1, arr2)
-	fmt.Println(cars)
+
+	cars[4] = "Bentley"
+	fmt.Println(cars, emptyArr)
+	fmt.Println(newArr, len(newArr), cap(newArr))
+	fmt.Println(5 | 2)
+	fmt.Println(5 / (math.Pow(7, 0) - math.Pow(1002, 0)))
+	fmt.Println(5 << 2)
+
+	// Grade class calculator
+	var score int = rand.Intn(101);
+	var grade string
+
+	if score >= 70 {
+		grade = "A"
+	} else if score >= 60 {
+		grade = "B"
+	} else if score >= 50 {
+		grade = "C"
+	} else if score >= 45 {
+		grade = "D"
+	} else if score >= 40 {
+		grade = "E"
+	} else if score < 40 {
+		grade = "F"
+	} else {
+		grade = "Invalid score"
+	}
+
+	fmt.Printf("Your grade is %s \n", grade)
+
+	nums := []int{1,2,3,4,5,6,7,8,9, 10}
+
+	for i, j := range nums {
+		fmt.Println(i, j)
+	}
+
+
+
+	multipleArgsFunc(8, 2, 3, 4)
 }
